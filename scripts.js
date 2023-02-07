b5c01a4b10cf272e99cd71c7adf00854346fec52
Sufyan Khan <skhan943@uwo.ca>
2023-02-07 13:16:16 -0500

    Update Styling

--- scripts.js
+++ scripts.js
@@ -1,17 +1,8 @@
 // Using window.onload to make sure all DOM objects have loaded before code runs
 window.onload = () => {
   // Add event listeners
-  let smallTip = document.getElementsByClassName("tip-button");
-  for (btn of smallTip) {
-    console.log(btn);
-    if (btn.id) {
-      btn.addEventListener("click", () => {
-        alert("Thank you!");
-      });
-    } else {
-      btn.addEventListener("click", () => {
-        alert("Sorry, that didn't work! Please try again.");
-      });
-    }
-  }
+  let tipBtn = document.getElementById("last");
+  tipBtn.addEventListener("click", () => {
+    alert("Thank you!");
+  });
 };
